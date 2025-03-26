ea884fb25d49b401c237a8226bb871c0cab54485
raghumadambath <38557203+raghumadambath@users.noreply.github.com>
2025-03-26 22:33:58 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+// Functions to toggle between pages
+function showProgramDetails() {
+  document.getElementById('main-page').classList.add('hidden');
+  document.getElementById('program-details').classList.remove('hidden');
+}
+
+function showMainPage() {
+  document.getElementById('program-details').classList.add('hidden');
+  document.getElementById('main-page').classList.remove('hidden');
+}
+
+// Function to scroll to current day
+function goToTodaysProgram() {
+  // First show the program details page
+  document.getElementById('main-page').classList.add('hidden');
+  document.getElementById('program-details').classList.remove('hidden');
+  
+  // Then scroll to the current day section
+  const currentDayElement = document.getElementById('current-day');
+  if (currentDayElement) {
+    setTimeout(() => {
+      currentDayElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+    }, 100);
+  }
+}
+
+// Will add more functionality later
